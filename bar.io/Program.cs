568a7c8b2ede2761c90f86064456c32c94be4712
Николов Бобан <boban.nikolov@students.finki.ukim.mk>
2021-05-09 23:29:49 +0200

    login form

--- bar.io/Program.cs
+++ bar.io/Program.cs
@@ -16,7 +16,6 @@ namespace bar.io
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
         }
     }
 }
