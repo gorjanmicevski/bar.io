2a356b701ecbe804c08d4b99178c2a1265fad432
Boban <56995138+BobanNikolov@users.noreply.github.com>
2021-05-21 13:37:00 +0200

    fixed validation bugs and added PasswordChar

---
+++ bar.io/Rezervacija.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bar.io
+{
+    public class Rezervacija
+    {
+        public string Name { set; get; }
+        public Masa Table { set; get; }
+
+        public Rezervacija(string name, Masa m)
+        {
+            Name = name;
+            Table = m;
+        }
+    }
+}
