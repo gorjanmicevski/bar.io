89ee9a4baceb26a1ad35e73bdaa60c2cc121b70f
Boban <56995138+BobanNikolov@users.noreply.github.com>
2021-06-08 12:59:24 +0200

    Uploaded new update for bar.io

---
+++ bar.io/DodadiMasa.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bar.io
+{
+    public partial class DodadiMasa : Form
+    {
+        public Masa myMasa;
+        public DodadiMasa()
+        {
+            InitializeComponent();
+        }
+        private void btnDodadi_Click(object sender, EventArgs e)
+        {
+            myMasa = new Masa(int.Parse(tbID.Text));
+            DialogResult = DialogResult.OK;
+        }
+
+        private void btnOtkazi_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+        }
+    }
+}
---
+++ bar.io/PocetnaMenadzer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bar.io
+{
+    public partial class PocetnaMenadzer : Form
+    {
+        public Menadzer menadzer;
+        public PocetnaMenadzer(Menadzer m)
+        {
+            menadzer = m;
+            InitializeComponent();
+        }
+
+        private void PocetnaMenadzer_Load(object sender, EventArgs e)
+        {
+            label2.Text = String.Format("Добредојдовте {0}!", menadzer.Name);
+        }
+
+        private void monthCalendar1_DateSelected(object sender, DateRangeEventArgs e)
+        {
+            String date = monthCalendar1.SelectionRange.Start.ToShortDateString();
+            RabotiNaDen forma = new RabotiNaDen(date);
+            if (forma.ShowDialog() == DialogResult.OK)
+            {
+
+            }
+        }
+
+        private void btnGoBack_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+        }
+    }
+}
