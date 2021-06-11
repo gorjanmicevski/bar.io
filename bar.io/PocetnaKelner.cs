04fb15991ff2c54180c8df65eb624cec70936acb
gorjanmicevski <83927100+gorjanmicevski@users.noreply.github.com>
2021-06-12 01:26:49 +0200

    Add files via upload

---
+++ bar.io/PocetnaKelner.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bar.io
+{
+    public partial class PocetnaKelner : Form
+    {
+        public Kelner kelner;
+        public PocetnaKelner(Kelner k)
+        {
+            kelner = k;
+            if (k == null)
+            {
+                kelner = new Kelner(k.Name);
+            }
+            InitializeComponent();
+        }
+
+        
+        private void PocetnaKelner_Load(object sender, EventArgs e)
+        {
+            lbMasi.Items.Clear();
+            foreach (Masa m in kelner.Tables)
+            {
+                lbMasi.Items.Add(m);
+            }
+        }
+
+        private void btnDodadiMasa_Click(object sender, EventArgs e)
+        {
+            DodadiMasa forma = new DodadiMasa();
+            if(forma.ShowDialog() == DialogResult.OK)
+            {
+                Masa m = forma.myMasa;
+                bool flag = false;
+                foreach(Masa item in kelner.Tables)
+                {
+                    if (item.Id==m.Id)
+                    {
+                        flag = true;
+                        break;
+                    }
+                }
+                if (flag)
+                {
+                    MessageBox.Show("Масате веќе постои!", "Неуспешно", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+                else
+                {
+                    kelner.Tables.Add(m);
+                    DodadiNaracka forma1 = new DodadiNaracka(m,kelner);
+                    if (forma1.ShowDialog() == DialogResult.OK)
+                    {
+                        lbMasi.Items.Add(m);
+                    }
+                    
+                }
+            }
+        }
+
+        private void btnGoBack_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+        }
+
+        private void btnIzbrisiMasa_Click(object sender, EventArgs e)
+        {
+            if (lbMasi.SelectedItem!=null)
+            {
+                VnesiLozinkaMenadzer forma = new VnesiLozinkaMenadzer(Korisnici.Menadzeri);
+                if (forma.ShowDialog() == DialogResult.OK)
+                {
+                    int index = lbMasi.SelectedIndex;
+                    Masa m = (Masa)lbMasi.Items[index];//***
+                    lbMasi.Items.RemoveAt(index);
+                    kelner.Tables.Remove(m);//****
+                    DialogResult = DialogResult.OK;
+                }
+                
+            }
+            else
+            {
+                MessageBox.Show( "Немате селектирано маса!", "грешка!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+        }
+
+        private void btnEditMasa_Click(object sender, EventArgs e)
+        {
+            if (lbMasi.SelectedItem != null)
+            {
+                Masa selektirana = (Masa)lbMasi.SelectedItem;
+                DodadiNaracka forma1 = new DodadiNaracka(selektirana,kelner);
+                if (forma1.ShowDialog() == DialogResult.OK)
+                {
+
+                }
+                /* DodadiMasa m = new DodadiMasa();
+                 m.tableToEdit((Masa)lbMasi.SelectedItem);
+                 if (m.ShowDialog() == DialogResult.OK)
+                 {
+                     lbMasi.SelectedItem =(Masa) m.myMasa;
+
+                     this.UpdateList();
+                 }
+                */
+            }
+            else
+            {
+                MessageBox.Show("Немате селектирано маса!", "грешка!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+        }
+        private void UpdateList()
+        {
+            lbMasi.Items.Clear();
+            foreach(Masa m in kelner.Tables)
+            {
+                lbMasi.Items.Add(m);
+            }
+        }
+    }
+}
+                
---
+++ bar.io/PocetnaSanker.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bar.io
+{
+    public partial class PocetnaSanker : Form
+    {
+        List<Kelner> kelneri;
+        public PocetnaSanker()
+        {
+            InitializeComponent();
+            kelneri = Pocetna.kelneri;
+            
+        }
+
+      
+
+        private void PocetnaSanker_Load(object sender, EventArgs e)
+        {
+            foreach(Kelner k in kelneri)
+            {
+                foreach(Masa m in k.Tables)
+                {
+
+                    lbNarackiZaIsporaka.Items.Add(m.Naracka);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (lbNarackiZaIsporaka.SelectedIndex != -1)
+            {
+                Naracka selected = (Naracka)lbNarackiZaIsporaka.SelectedItem;
+                Isporaka form = new Isporaka();
+                if (form.ShowDialog() == DialogResult.OK)
+                {
+                    if (lbNarackiZaIsporaka.SelectedItem != null)
+                    {
+                        selected.aktivna = false;
+                        lbNarackiZaIsporaka.Items.Remove(selected);
+                    }
+                    //Dve opcii: koga kje se isporaca da se izvadi od lista ili da se stavi aktivna=false i da stoi..
+                    //Prva opcija lesna
+                    //Vtora opcija ne mi teknuva kako se prai refresh na lista za da se updatenat narackite i da se smeni aktivna vo false
+
+
+                    //lbNarackiZaIsporaka.Items.Add(selected);
+                    //lbNarackiZaIsporaka.Refresh();
+                }
+            }
+        }
+    }
+}
